f87b1cbcde15274c4cf78efeeeafe397207582e0
LindgrenArto <arto.lindgren1@gmail.com>
2024-11-13 09:19:35 +0200

    Refactored repository level to work with DTO Models

--- UsersTestApi/Repositories/UserRepository.cs
+++ UsersTestApi/Repositories/UserRepository.cs
@@ -7,8 +7,7 @@ public class UserRepository : IUserRepository
 {
     private readonly IMongoCollection<User> _users;
 
-    public UserRepository(
-           IOptions<UserDatabaseSettings> userDatabaseSettings)
+    public UserRepository(IOptions<UserDatabaseSettings> userDatabaseSettings)
     {
         var mongoClient = new MongoClient(
             userDatabaseSettings.Value.ConnectionString);
@@ -21,21 +20,108 @@ public class UserRepository : IUserRepository
     }
 
     // Get all users
-    public async Task<List<User>> GetAllUsersAsync()
+    public async Task<List<UserDTO>> GetAllUsersAsync()
     {
-        return await _users.Find(_ => true).ToListAsync();
+        var users = await _users.Find(_ => true).ToListAsync();
+
+        return users.Select(u => new UserDto
+        {
+            Id = u.Id,
+            Name = u.Name,
+            Username = u.Username,
+            Email = u.Email,
+            Phone = u.Phone,
+            Website = u.Website,
+            Address = new AddressDto
+            {
+                Street = u.Address.Street,
+                Suite = u.Address.Suite,
+                City = u.Address.City,
+                Zipcode = u.Address.Zipcode,
+                Geo = new GeoDTO
+                {
+                    Lat = u.Address.Geo.Lat,
+                    Lng = u.Address.Geo.Lng
+                }
+            },
+            Company = new CompanyDto
+            {
+                Name = u.Company.Name,
+                CatchPhrase = u.Company.CatchPhrase,
+                Bs = u.Company.Bs
+            }
+        }).ToList();
     }
 
     // Get a user by ID
     public async Task<User> GetUserByIdAsync(int id)
     {
-        return await _users.Find(user => user.Id == id).FirstOrDefaultAsync();
+        var user = await _users.Find(u => u.Id == id).FirstOrDefaultAsync();
+        if (user == null)
+            return null;
+
+        return new UserDto
+        {
+            Id = user.Id,
+            Name = user.Name,
+            Username = user.Username,
+            Email = user.Email,
+            Phone = user.Phone,
+            Website = user.Website,
+            Address = new AddressDto
+            {
+                Street = user.Address.Street,
+                Suite = user.Address.Suite,
+                City = user.Address.City,
+                Zipcode = user.Address.Zipcode,
+                Geo = new GeoDTO
+                {
+                    Lat = u.Address.Geo.Lat,
+                    Lng = u.Address.Geo.Lng
+                }
+            },
+            Company = new CompanyDto
+            {
+                Name = user.Company.Name,
+                CatchPhrase = user.Company.CatchPhrase,
+                Bs = user.Company.Bs
+            }
+        };
     }
 
     // Update an existing user
-    public async Task<bool> UpdateUserAsync(User user)
+    public async Task<bool> UpdateUserAsync(UserDTO userDTO)
     {
-        var result = await _users.ReplaceOneAsync(u => u.Id == user.Id, user);
+        var user = await _users.Find(u => u.Id == userDTO.Id).FirstOrDefaultAsync();
+        if (user == null)
+            return false;
+
+        // Map back from DTO to the actual model
+        user.Name = userDTO.Name;
+        user.Username = userDTO.Username;
+        user.Email = userDTO.Email;
+        user.Phone = userDTO.Phone;
+        user.Website = userDTO.Website;
+        user.Address = new Address
+        {
+            Street = userDTO.Address.Street,
+            Suite = userDTO.Address.Suite,
+            City = userDTOAddress.City,
+            Zipcode = userDTO.Address.Zipcode
+            Geo = new Geo
+            {
+                Lat = userDTO.Address.Geo.Lat,
+                Lng = userDTO.Addresss.Geo.Lng
+            }
+        };
+        user.Company = new Company
+        {
+            Name = userDTO.Company.Name,
+            CatchPhrase = userDTO.Company.CatchPhrase,
+            Bs = userDTO.Company.Bs
+        };
+
+        var result = await _users.ReplaceOneAsync(u => u.Id == userDTO.Id, user);
         return result.IsAcknowledged && result.ModifiedCount > 0;
     }
 
@@ -46,3 +132,4 @@ public class UserRepository : IUserRepository
         return result.IsAcknowledged && result.DeletedCount > 0;
     }
 }
+
