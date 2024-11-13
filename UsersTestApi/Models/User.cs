fe82a352d19191820e36d73c826f0604c7fcb905
LindgrenArto <arto.lindgren1@gmail.com>
2024-11-13 09:07:08 +0200

    Refactored models to include namespace, added DTO models

---
+++ UsersTestApi/Models/User.cs
@@ -0,0 +1,19 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace UserTestApi.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public Company Company { get; set; }
+    }
+}
